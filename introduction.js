5fde3979dc64b01a33fd6c3b335eecdf6d55443d
pan93412 <pan93412@gmail.com>
2019-09-14 17:33:22 +0800

    feat(intro): add introduction text

---
+++ introduction.js
@@ -0,0 +1,8 @@
+// introduction text
+const intro = [
+  "_Hello!_ It is a message from **TMEditor 1.5**! The new version of TMEditor added a ",
+  "feature that let you can preview `Markdown` documents **in live**!! ",
+  " you can preview this at [this link](https://pan93412.github.io/TMEditor/TMEditor.en_US.html), ",
+  "also, you can edit this introduction text. click [clear] to clear those. :)\n\n",
+  "_pan93412, 2019. All rights are not preversed._`",
+].join('')
